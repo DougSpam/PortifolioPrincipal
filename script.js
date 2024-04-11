57192dd06c4cbbeebd3689489c699c5ac2a51814
DougSpam <ddougllasbezerra@gmail.com>
2024-04-11 19:13:17 -0300

    a

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ window.onload = function() {
     var videoElement = document.getElementById('video-background');
     var isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
-    if (isiOS) {
-        videoElement.parentNode.removeChild(videoElement);
-    } else {
-    }
+    window.onload = function() {
+        var videoElement = document.getElementById('video-background');
+        var isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    
+        if (isiOS) {
+            videoElement.src = document.getElementById('ios-video').src;
+        } else {
+            videoElement.src = document.getElementById('android-video').src;
+        }
+    };
+    
 };
